9ce2e795c4f00a954760181a25367afd638b2bfd
matheuscch <matheuscamposdecarvalho@gmail.com>
2022-04-13 16:09:18 -0300

    adding leads services

---
+++ src/services/RegisterLeadService.ts
@@ -0,0 +1,28 @@
+import { getCustomRepository } from "typeorm"
+import { LeadsRepositories } from "../repositories/LeadsRepositories"
+import { UserRepositories } from "../repositories/UsersRepositories"
+
+interface ILeadRequest {
+    email: string,    
+    name:string,
+    is_closed: boolean,
+    user_id: string
+}
+
+class RegisterLeadService {
+    async execute({email,name,is_closed,user_id} : ILeadRequest){
+        const leadsRepository = getCustomRepository(LeadsRepositories)
+   
+        
+        const lead = leadsRepository.create({
+            email,
+            name,
+            is_closed,
+            user_id
+        })
+        
+        await leadsRepository.save(lead)
+        return lead
+    }
+}
+export {RegisterLeadService}
