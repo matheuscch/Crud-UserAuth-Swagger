76e8051187b85be39b1cf6e0e2f07da529dc61d1
matheuscch <matheuscamposdecarvalho@gmail.com>
2022-04-13 15:31:48 -0300

    adding swagger basic config

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express,{Request,Response,NextFunction} from "express"
 import "express-async-errors"
 import "reflect-metadata"
 import "./database"
+import swaggerUi from "swagger-ui-express"
 import {router} from "./routes"
+import swaggerDocs from "./swagger.json"
 const app = express()
 app.use(express.json())
-app.use(router)
+app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerDocs))
 app.use((err: Error, request: Request , response : Response,next:NextFunction)=> {
     if(err instanceof Error) {
         return response.status(400).json({
@@ -17,4 +19,11 @@ app.use((err: Error, request: Request , response : Response,next:NextFunction)=>
         message: "Internal Server Error"
             })
     })
+
+    app.get("/terms", (request,response) =>{
+        return response.json({
+            message:"Termos de Serviço",
+        })
+    })
+app.use("/api",router)
 app.listen(3000,()=>console.log("Running"))
